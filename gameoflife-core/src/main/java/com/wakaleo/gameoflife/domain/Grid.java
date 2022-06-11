f99e4bbf8b4c0d28e916c643b41200fa59af7f3d
nafanjka <stas.hrytsyk@gmail.com>
2022-06-11 09:12:09 +0300

    change grid

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Grid.java
@@ -12,8 +12,8 @@ import static com.wakaleo.gameoflife.domain.Cell.LIVE_CELL;
  */
 public class Grid {
 
-    private static final int DEFAULT_ROW_COUNT = 3;
-    private static final int DEFAULT_COLUMN_COUNT = 3;
+    private static final int DEFAULT_ROW_COUNT = 5;
+    private static final int DEFAULT_COLUMN_COUNT = 10;
 
     private Cell[][] cells;
 
